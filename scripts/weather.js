c3e0078d427a62aa8a0bb12ddb86591c483a2773
GuinhoSensei <thiggouveia@hotmail.com>
2024-03-14 18:35:01 -0700

    home page updated

--- scripts/weather.js
+++ scripts/weather.js
@@ -2,14 +2,17 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('#figcaption');
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=imperial&appid=deec6a3c399e4cb60e5ccaf8aa746390';
+const lat =37.98981744679719;
+const lon =-122.3274832236111;
+const key ='deec6a3c399e4cb60e5ccaf8aa746390';
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${key}`;
 
 async function apiFetch(){
     try{
         const response = await fetch(url);
         if(response.ok){
             const data = await response.json();
-            //console.log(data);
+            console.log(data);
             displayResults(data);
         } else{
             throw Error(await response.text());
